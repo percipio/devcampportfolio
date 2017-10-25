0740f067c9bedc293f5c0a0fba27f09d9863810b
Joe Hale <me@joehale.io>
2017-10-24 19:39:54 -0700

    Configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
 
-  get 'pages/about'
-
-  get 'pages/contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
   resources :blogs
+
+  root to: 'pages#home'
 end
